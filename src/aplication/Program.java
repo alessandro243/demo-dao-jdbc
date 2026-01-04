3886492178880d0407e00143619eaecf31d73409
alessandro243 <alex.alessandro243@gmail.com>
2026-01-04 01:50:38 -0300

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,10 @@
+package aplication;
+
+import entities.Department;
+
+public class Program {
+    public static void main(String[] args){
+        Department dp = new Department(1, "Books");
+        System.out.println(dp);
+    }
+}
